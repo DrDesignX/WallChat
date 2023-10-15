aee1b2eea1011b104a7ababf81313e1b201e6beb
MohamadAmin Seydi <mr.seadi@gmail.com>
2023-10-16 00:06:29 +0330

    add Notification for Join and left

--- Server/main.go
+++ Server/main.go
@@ -24,18 +24,15 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-
 	defer serverListener.Close()
 
 	fmt.Println("Server is listening on 127.0.0.1:12345")
-
 	for {
 		clientConn, err := serverListener.AcceptTCP()
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-
 		// Handle each connection in a separate Goroutine
 		go handleConnection(clientConn)
 	}
@@ -47,16 +44,17 @@ func handleConnection(conn *net.TCPConn) {
 	buf := make([]byte, 1024)
 	client := Client{conn: conn}
 
-	// Prompt the user for a username
 	client.username = getUsername(conn)
 	clients = append(clients, client)
+	notify(client, "has joined the chat.\n")
 	fmt.Printf("%s has joined the chat.\n", client.username)
 
 	for {
 		// Read data from the client
 		n, err := conn.Read(buf)
 		if err != nil {
-			fmt.Printf("%s has left the chat.\n", client.username)
+			notify(client, "has left the chat.\n")
+
 			removeClient(client)
 			return
 		}
@@ -64,13 +62,22 @@ func handleConnection(conn *net.TCPConn) {
 		receivedMessage := string(buf[:n])
 		fmt.Printf("%s: %s", client.username, receivedMessage)
 
-		// Broadcast the message to all clients
+		// Broadcast
 		broadcastMessage(client, receivedMessage)
 	}
 }
 
+func notify(sender Client, message string) {
+	for _, client := range clients {
+		if client.conn != sender.conn {
+			client.conn.Write([]byte(sender.username + " " + message))
+			return
+		}
+	}
+}
+
 func getUsername(conn *net.TCPConn) string {
-	conn.Write([]byte("Enter your username: "))
+	// conn.Write([]byte("Enter your username: "))
 	buf := make([]byte, 1024)
 	n, _ := conn.Read(buf)
 	return string(buf[:n])
